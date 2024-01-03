4450aaf0a9b58111f89191f1c60b16f5abb6b8c3
Valeriia Lyakisheva <vorik07@icloud.com>
2024-01-03 16:37:05 +0100

    leave message displays in a proper  place

--- src/components/ConversationDetail.tsx
+++ src/components/ConversationDetail.tsx
@@ -1,51 +1,72 @@
 import { useContext, useState } from "react";
-import { ConversationContext, MessageI, ParticipationEventsI } from "../contexts/ConversationContext";
+import {
+  ConversationContext,
+  MessageI,
+  ParticipationEventsI,
+} from "../contexts/ConversationContext";
 import ConversationInputForm from "./ConversationInputForm";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faTrashCan,
   faPen,
   faSquareCheck,
-  faArrowRightFromBracket
+  faArrowRightFromBracket,
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function ConversationDetail() {
-  const { currentConversation, messages, participationEvents, users, addParticipationEvent, userId } = useContext(ConversationContext);
+  const {
+    currentConversation,
+    messages,
+    participationEvents,
+    users,
+    addParticipationEvent,
+    userId,
+  } = useContext(ConversationContext);
 
-  // useEffect(()=> {
-  //   const messagesPopulatedWithNames = messages.map(m => m.user = users.find(u => u.id === m.user_id))
-  //   const messagesGroupedByAuthor = []
-  //   messagesPopulatedWithNames.forEach((m,i)=> {
-  //     if(messagesGroupedByAuthor.length === 0) {
-  //       messagesGroupedByAuthor.push([m])
-  //     } else {
-  //       if(m.user_id === messagesGroupedByAuthor[messagesGroupedByAuthor.length-1].user_id) {
-  //         messagesGroupedByAuthor[messagesGroupedByAuthor.length-1].push(m)
-  //       } else {
-  //         messagesGroupedByAuthor.push([m])
-  //       }
-  //     }
-  //   })
-  // }, [messages])
-
-  let lastPartEventsUniqUserId: Record<string, ParticipationEventsI> = {} 
-  for( let i = 0; i < participationEvents.length; i++) {
-    let currentEvent = participationEvents[i]
-    let userIdInEvent = currentEvent.user_id
-      lastPartEventsUniqUserId[userIdInEvent] = currentEvent
+  let lastPartEventsUniqUserId: Record<string, ParticipationEventsI> = {};
+  for (let i = 0; i < participationEvents.length; i++) {
+    let currentEvent = participationEvents[i];
+    let userIdInEvent = currentEvent.user_id;
+    lastPartEventsUniqUserId[userIdInEvent] = currentEvent;
   }
 
-  let filteredEventsArray = Object.values(lastPartEventsUniqUserId)
+  let filteredEventsArray = Object.values(lastPartEventsUniqUserId);
 
-const messagesArray = [...messages];
-const eventsArray = [...filteredEventsArray];
+  const messagesArray = [...messages];
+  const eventsArray = [...filteredEventsArray];
 
-// Sort messages and events based on the created_at property
-const allEvents = [...messagesArray, ...eventsArray]
-allEvents.sort((a, b) => {
-  return Number(a.created_at) - Number(b.created_at);
-});
+  // Sort messages and events based on the created_at property
+  const sortedMessages = messagesArray.sort(
+    (a, b) => Number(a.created_at) - Number(b.created_at)
+  );
+  const sortedEvents = eventsArray.sort(
+    (a, b) => Number(a.created_at) - Number(b.created_at)
+  );
 
+  function mergeMessagesAndEvents(
+    messages: MessageI[],
+    events: ParticipationEventsI[]
+  ): (MessageI | ParticipationEventsI)[] {
+    const mergedArray: (MessageI | ParticipationEventsI)[] = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < messages.length || j < events.length) {
+      if (i < messages.length) {
+        mergedArray.push(messages[i]);
+        i++;
+      }
+      if (j < events.length) {
+        mergedArray.push(events[j]);
+        j++;
+      }
+    }
+
+    return mergedArray;
+  }
+
+  // Merge messages and events in the correct order
+  const allEvents = mergeMessagesAndEvents(sortedMessages, sortedEvents);
 
   return (
     <div className="p-2 message-container">
@@ -53,25 +74,39 @@ allEvents.sort((a, b) => {
         <>
           <div>
             <p className="inline mr-4">{currentConversation.topic}</p>
-            <button onClick={() => {
-                  addParticipationEvent(userId, currentConversation.id)
-                }}><FontAwesomeIcon icon={faArrowRightFromBracket} />
-                </button>
+            <button
+              onClick={() => {
+                addParticipationEvent(userId, currentConversation.id);
+              }}
+            >
+              <FontAwesomeIcon icon={faArrowRightFromBracket} />
+            </button>
           </div>
-            {filteredEventsArray.map((partEvent) => 
-                partEvent.participant === true && users.map((user) => {
-                  if(partEvent.user_id === user.id) {
-                    return (
+          {filteredEventsArray.map(
+            (partEvent) =>
+              partEvent.participant === true &&
+              users.map((user) => {
+                if (partEvent.user_id === user.id) {
+                  return (
                     <ul>
-                      <li><b>{user.name}</b></li>
+                      <li>
+                        <b>{user.name}</b>
+                      </li>
                     </ul>
-                    )
-                  }})
-            )}    
+                  );
+                }
+              })
+          )}
           <div>
             {allEvents.map((event, index) => (
               <div key={event.id}>
-                <Message event={event as MessageI} isFirstInGroup={index === 0 || allEvents[index - 1].user_id !== event.user_id} />
+                <Message
+                  event={event as MessageI}
+                  isFirstInGroup={
+                    index === 0 ||
+                    allEvents[index - 1].user_id !== event.user_id
+                  }
+                />
               </div>
             ))}
           </div>
@@ -84,94 +119,106 @@ allEvents.sort((a, b) => {
   );
 }
 
-function Message({ event, isFirstInGroup }: {event: MessageI, isFirstInGroup: boolean}) {
-  const { updateMessage, deleteMessage, userId, users } = useContext(ConversationContext);
+function Message({
+  event,
+  isFirstInGroup,
+}: {
+  event: MessageI;
+  isFirstInGroup: boolean;
+}) {
+  const { updateMessage, deleteMessage, userId, users } =
+    useContext(ConversationContext);
   const [isEditing, setIsEditing] = useState(false);
 
   function unescape(string: string) {
-    const a = new DOMParser()
-    const b = a.parseFromString(string,'text/html')
-    const c = b.querySelector('html')
-    const d = c?.textContent
-    return d
+    const a = new DOMParser();
+    const b = a.parseFromString(string, "text/html");
+    const c = b.querySelector("html");
+    const d = c?.textContent;
+    return d;
   }
 
   let messagesContent;
 
-  if ('content' in event) {
+  if ("content" in event) {
     // It's a message
     const message = event as MessageI;
 
-  if (isEditing) {
-    messagesContent = (
-      <div key={message.id}>
-        <input
-          value={message.content}
-          onChange={(e) => {
-            updateMessage({
-              ...message,
-              content: e.target.value,
-            });
-          }}
-        />
-        <button
-          onClick={() => {
-            setIsEditing(false);
-          }}
-        >
-          <FontAwesomeIcon icon={faSquareCheck} />
-        </button>
-      </div>
-    );
-  } else if(message){
-    messagesContent = (
-      <div key={message.id}>
-        {unescape(message.content)}
-        <button 
-        onClick={() => {
-          if(message.user_id === userId) {
-            setIsEditing(true)};
-        }}>
-          <FontAwesomeIcon icon={faPen} />
-        </button>
-        <button 
-          className="my-2"
-          onClick={() => {
-            if(message.user_id === userId) {
-            deleteMessage(message.id);}
-          }}
-        >
-          <FontAwesomeIcon icon={faTrashCan} />
-        </button>
-      </div>
-    );
-  }
-
-  let userName = users.map((user) => {
-    return (
-      message.user_id === user.id && 
-      <p>{user.name}</p>
-    ) 
-  }) 
-
-  return (
+    if (isEditing) {
+      messagesContent = (
         <div key={message.id}>
-          <div>
-            <p>{isFirstInGroup?<b>{userName}</b>:""}{messagesContent}</p>
-          </div>
-      </div>
-    )
-} else {
-  // It's a participation event
-  const eventParticipation = event as ParticipationEventsI;
-  const user = users.find((u) => u.id === eventParticipation.user_id);
+          <input
+            value={message.content}
+            onChange={(e) => {
+              updateMessage({
+                ...message,
+                content: e.target.value,
+              });
+            }}
+          />
+          <button
+            onClick={() => {
+              setIsEditing(false);
+            }}
+          >
+            <FontAwesomeIcon icon={faSquareCheck} />
+          </button>
+        </div>
+      );
+    } else if (message) {
+      messagesContent = (
+        <div key={message.id}>
+          {unescape(message.content)}
+          <button
+            onClick={() => {
+              if (message.user_id === userId) {
+                setIsEditing(true);
+              }
+            }}
+          >
+            <FontAwesomeIcon icon={faPen} />
+          </button>
+          <button
+            className="my-2"
+            onClick={() => {
+              if (message.user_id === userId) {
+                deleteMessage(message.id);
+              }
+            }}
+          >
+            <FontAwesomeIcon icon={faTrashCan} />
+          </button>
+        </div>
+      );
+    }
 
-  if (eventParticipation.participant === false) {
+    let userName = users.map((user) => {
+      return message.user_id === user.id && <p>{user.name}</p>;
+    });
+
     return (
-      <div key={eventParticipation.id}>
-        <p><b>{user?.name} left the conversation</b></p>
+      <div key={message.id}>
+        <div>
+          <p>
+            {isFirstInGroup ? <b>{userName}</b> : ""}
+            {messagesContent}
+          </p>
+        </div>
       </div>
     );
-  } 
-}
+  } else {
+    // It's a participation event
+    const eventParticipation = event as ParticipationEventsI;
+    const user = users.find((u) => u.id === eventParticipation.user_id);
+
+    if (eventParticipation.participant === false && user) {
+      return (
+        <div key={eventParticipation.id}>
+          <p>
+            <b>{user.name} left the conversation</b>
+          </p>
+        </div>
+      );
+    }
+  }
 }
