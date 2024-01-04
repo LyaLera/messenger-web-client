fc2b3e5ee9b089793084b2f33942e6ce86ea1b4b
Valeriia Lyakisheva <vorik07@icloud.com>
2024-01-04 10:18:32 +0100

    Change path

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import Conversation from "./components/Conversation"
 import Navbar from "./components/Navbar"
-import Login from "./components/Login"
+//import Login from "./components/Login"
 import { Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Login />}/>
-      <Route path="/conversations/:id?" element={[<Navbar/>, <Conversation />]}/>
+      {/* <Route path="/" element={<Login />}/> */}
+      <Route path="/" element={[<Navbar/>, <Conversation />]}/>
     </Routes>
   )
 }
