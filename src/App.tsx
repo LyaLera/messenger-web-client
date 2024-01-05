8346b9d6729a0baea9dcc2f12855d01860a99ea6
Valeriia Lyakisheva <vorik07@icloud.com>
2024-01-05 10:17:05 +0100

    witoutt login page for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import Conversation from "./components/Conversation"
 import Navbar from "./components/Navbar"
-import Login from "./components/Login"
+//import Login from "./components/Login"
 import { Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Login />}/>
+      {/* <Route path="/" element={<Login />}/> */}
       <Route path="/conversations/:id" element={[<Navbar/>, <Conversation />]}/>
     </Routes>
   )
